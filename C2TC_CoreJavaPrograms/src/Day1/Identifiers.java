84cfa2e989b0e59ba399dcd8c3d89bb3aa0fb254
THAMIZH221909 <dthamizhselvi2004@gmail.com>
2025-08-15 10:12:59 +0530

    Commit

---
+++ C2TC_CoreJavaPrograms/src/Day1/Identifiers.java
@@ -0,0 +1,15 @@
+package Day1;
+
+public class Identifiers {
+	public static void main(String[] args) {
+		
+		int $number = 20;    //starts with $
+		System.out.println("value of the number variable is : "+ $number);
+	
+		String studentName="Selvi";    //use camelcase
+		System.out.println("value is : "+ studentName);
+		
+	
+	}
+
+}
