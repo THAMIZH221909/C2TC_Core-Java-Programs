bfce18b4c645b0a09c504f7ed8795252f0909e6b
THAMIZH221909 <dthamizhselvi2004@gmail.com>
2025-08-15 21:22:31 +0530

    Commit

---
+++ C2TC_CoreJavaPrograms/src/Day3/Encapsulation/Encapsulation.java
@@ -0,0 +1,15 @@
+package Day3.Encapsulation;
+
+public class Encapsulation {
+public static void main(String[] args) {
+		
+		//object creation 
+		OopsConcept obj = new OopsConcept();
+		obj.setSerialNum(101);
+		obj.setName("Pooja");
+		obj.setAge(20);
+		System.out.println(obj);
+		
+	}
+
+}
